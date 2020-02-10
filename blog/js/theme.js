38de1b30da4304e76ff021261be624cb4f2fb697
Egor977 <55953720+Egor977@users.noreply.github.com>
2020-02-10 20:42:11 +0500

    blog v2

--- blog/js/theme.js
+++ blog/js/theme.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".header__menu-item a").each(function(){window.location.href==this.href&&$(this).parent().addClass("active")})});
+$(document).ready(function(){$(".header__menu-item a").each(function(){window.location.href==this.href&&$(this).parent().addClass("active")}),$("a").click(function(){var t=$(this).attr("href");if("#"==t)return $("html, body").animate({scrollTop:0},600),!1;var a=$(t).offset().top;return $("html, body").animate({scrollTop:a},600),!1})});
